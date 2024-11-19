80a1beb41ef6a611e5fde3c5ff07bdf3cc6b908e
songhyeongju <hyeongah9@gmail.com>
2024-11-19 15:06:18 +0900

    tuple

---
+++ tuple.py
@@ -0,0 +1,12 @@
+# 한번 생성하면 삽입하거나 삭제할 수 없음
+my_tuple = (1, 2, 3)
+
+# 인덱싱
+print(my_tuple[0])
+print(my_tuple[1])
+print(my_tuple[2])
+
+# 슬라이싱
+print(my_tuple[1:])
+print(my_tuple[:2])
+print(my_tuple[1:2])
