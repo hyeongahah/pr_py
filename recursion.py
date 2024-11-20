3309849e4cb9f201ebf1ac46a5afb3d958e2e3db
songhyeongju <hyeongah9@gmail.com>
2024-11-20 16:30:14 +0900

    debug

--- recursion.py
+++ recursion.py
@@ -1,7 +1,9 @@
 def add_to_10(i):
     if i == 10:  # 기저 사례 : 재귀 호출을 멈추는 조건
         return i
-    return i + add_to_10(i + 1)  # 재귀 사례 : i가 10보다 커질 때까지 반복
+    # return i + add_to_10(i + 1)  # 재귀 사례 : i가 10보다 커질 때까지 반복
+    sum = i + add_to_10(i + 1)  # 반환값을 변수에 저장해서 디버깅하기 쉽게 한다
+    return sum
 
 
 sum = add_to_10(1)
